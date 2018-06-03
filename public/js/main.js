21980506a5461b52caaa57fa3f402f634d6bc71f
deadastro <39497624+deadastro@users.noreply.github.com>
2018-06-03 19:00:37 +0300

    v 0.5
    Added preloader.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$('.navbar-toggler').on('click', function(){
+$('.navbar-toggler').on('click', function () {
     $(this).toggleClass('clicked');
     if ($(this).hasClass('clicked')) {
         $('.fas').removeClass('fa-bars').addClass('fa-times');
@@ -6,3 +6,14 @@ $('.navbar-toggler').on('click', function(){
         $('.fas').removeClass('fa-times').addClass('fa-bars');
     }
 });
+
+setTimeout(function () {
+    $(document).ready(function ($) {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    });
+    $(window).load(function () {
+        var Body = $('body');
+        Body.addClass('preloader-site');
+    });
+}, 300)
